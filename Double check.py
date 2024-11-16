cfaae4713de5a8f57fe280acb8181b31b8c66ab7
catarinaperei <smlilwana@gmail.com>
2024-11-16 14:46:08 +0200

    Double check

---
+++ Double check.py
@@ -0,0 +1,9 @@
+n=int(input("enter a number:"))
+if n>50:
+    if n%2==0:
+        print("the number is greater than 50 and even")
+    else:
+        print("the number is greater than 50 and odd")
+else:
+    print("the number is less than 50")
+       
